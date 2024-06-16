1b29b9deed95c851b7cd98d561ebe0229a8100cc
Siddesh-B <siddeshb8316@gmail.com>
2024-06-16 22:43:15 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+
+export {Hero,PopularProducts,CustomerReviews,Services,SpecialOffers,Subscribe,Footer,SuperQuality}
